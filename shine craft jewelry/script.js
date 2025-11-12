69387ac44515aa7d9b7103a8cac45dedeeef7be7
Wachia-code <2108977@students.kcau.ac.ke>
2025-11-13 00:24:18 +0300

    Add files via upload

---
+++ shine craft jewelry/script.js
@@ -0,0 +1,7 @@
+// For now: just a simple placeholder
+console.log("ShineCraft Jewelry site script loaded");
+
+// You can add functions for:
+// - “Add to Cart” button click
+// - Updating cart quantity
+// - Storing cart items in localStorage
